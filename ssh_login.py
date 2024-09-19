a27bb18133e3d874d67d2804f3d06f9c09b21a57
hugo-esquer <hugo.esquer@laplateforme.io>
2024-09-19 19:38:20 +0200

    script Job 01

--- ssh_login.py
+++ ssh_login.py
@@ -1,14 +1,11 @@
 import subprocess
-import os
 
 class SSHLogin:
     def __init__(self, username, host, port=22):
         self.username = username
         self.host = host
         self.port = port
-        self.passwd = os.getenv("SUDO_PASSWD")
         self.ssh_prompt = ["ssh", self.ssh_prompt, "-p", str(self.port)]
-        self.sudo_prompt = ["ssh", f"{self.username}@{self.host}", "-p", str(self.port)]
 
 
 
@@ -27,36 +24,6 @@ class SSHLogin:
         except Exception as e :
             print(f"Error : {e}")
 
-    def sudo_command(self, command):
-        try:
-            sudoCmd = self.sudo_prompt.append(f"echo {self.passwd} | sudo -S {command}")
-
-            result = subprocess.run(sudoCmd, capture_output=True, text=True)
-
-            if result.stdout:
-                print(result.stdout)
-            if result.stderr:
-                print("Error :")
-                print(result.stderr)
-
-        except Exception as e :
-            print(f"Error : {e}")
-    
-    def sql_command(self, command):
-        try:
-            ssh_command = ["ssh", f"{self.username}@{self.host}", "-p", str(self.port), f"echo {self.passwd} | sudo -S {command}"]
-
-            result = subprocess.run(ssh_command, capture_output=True, text=True)
-
-            if result.stdout:
-                print(result.stdout)
-            if result.stderr:
-                print("Error :")
-                print(result.stderr)
-
-        except Exception as e :
-            print(f"Error : {e}")
-
 if __name__ == "__main__" :
     username = "monitor"
     ftp = "192.168.140.101"
@@ -64,4 +31,3 @@ if __name__ == "__main__" :
     cnx_ftp = SSHLogin(username, ftp)
 
     cnx_ftp.ssh_command("ls -la")
-    cnx_ftp.sudo_command("ls -la")
