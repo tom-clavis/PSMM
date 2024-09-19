14c415916b32e774661433cf88ca689752a2f9ce
hugo-esquer <hugo.esquer@laplateforme.io>
2024-09-19 19:31:49 +0200

    script modification

--- ssh_login.py
+++ ssh_login.py
@@ -7,14 +7,16 @@ class SSHLogin:
         self.host = host
         self.port = port
         self.passwd = os.getenv("SUDO_PASSWD")
+        self.ssh_prompt = ["ssh", self.ssh_prompt, "-p", str(self.port)]
+        self.sudo_prompt = ["ssh", f"{self.username}@{self.host}", "-p", str(self.port)]
 
 
 
     def ssh_command(self, command):
         try:
-            ssh_command = ["ssh", f"{self.username}@{self.host}", "-p", str(self.port), command]
+            sshCmd = self.ssh_prompt.append(command)
 
-            result = subprocess.run(ssh_command, capture_output=True, text=True)
+            result = subprocess.run(sshCmd, capture_output=True, text=True)
 
             if result.stdout:
                 print(result.stdout)
@@ -26,6 +28,21 @@ class SSHLogin:
             print(f"Error : {e}")
 
     def sudo_command(self, command):
+        try:
+            sudoCmd = self.sudo_prompt.append(f"echo {self.passwd} | sudo -S {command}")
+
+            result = subprocess.run(sudoCmd, capture_output=True, text=True)
+
+            if result.stdout:
+                print(result.stdout)
+            if result.stderr:
+                print("Error :")
+                print(result.stderr)
+
+        except Exception as e :
+            print(f"Error : {e}")
+    
+    def sql_command(self, command):
         try:
             ssh_command = ["ssh", f"{self.username}@{self.host}", "-p", str(self.port), f"echo {self.passwd} | sudo -S {command}"]
 
